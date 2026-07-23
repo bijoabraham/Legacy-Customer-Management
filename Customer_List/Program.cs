a570278d1ad9fa11abb7c2e3219ad1edc8a0e885
Bijo Abraham <bijo.abraham@bytestrone.com>
2026-07-23 20:07:38 +0530

    Fix missing UI buttons by mapping static files to wwwroot and fix EF Core LocalDB collision

--- Customer_List/Program.cs
+++ Customer_List/Program.cs
@@ -3,10 +3,12 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+AppDomain.CurrentDomain.SetData("DataDirectory", System.IO.Path.Combine(builder.Environment.ContentRootPath, "App_Data"));
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<CustomersWebDemo.DbAccess.CustomerEntitiesDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("CustomersDB")));
 var app = builder.Build();
+app.UseStaticFiles();
 app.UseRouting();
 app.MapControllerRoute(
     name: "default",
